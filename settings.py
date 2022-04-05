98755dbb6adc63fd754c4a707c7228757a762443
Vinicius Arcanjo <viniarck@gmail.com>
2022-04-05 10:35:51 -0300

    Fixed LOOP_LOG_EVERY default value Added comment regarding LLDP_IGNORED_LOOPS on settings

--- settings.py
+++ settings.py
@@ -9,7 +9,9 @@ TOPOLOGY_URL = 'http://localhost:8181/api/kytos/topology/v3'
 
 LLDP_LOOP_ACTIONS = ["log"]  # supported actions ["log", "disable"]
 LLDP_IGNORED_LOOPS = {}  # ignored loops per dpid {"dpid": [[1, 2]]}
-LOOP_LOG_EVERY = int(max(300 / max(POLLING_TIME, 1), 1))  # 5 mins by default
+# LLDP_IGNORED_LOOPS can be overwritten by switch.metadata.ignored_loops
+
+LOOP_LOG_EVERY = int(max(900 / max(POLLING_TIME, 1), 1))  # 5 mins by default
 
 # Prefix this NApp has when using cookies
 COOKIE_PREFIX = 0xab
